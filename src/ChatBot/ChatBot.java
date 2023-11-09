675a32ad99b4c531d130b7a19f622d254a506ede
YehorZhuravko <y.a.zhuravko@student.khai.edu>
2023-11-09 02:48:06 +0200

    ChatBot 2nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,2 +1,18 @@
-package ChatBot;public class ChatBot {
+package ChatBot;
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args) {
+
+        // Вывод приветствия
+        System.out.println("Hello! My name is Test Chat Bot");
+        System.out.println("I was created in 2023");
+
+        // Ввод имени пользователя и вывод приветствия
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, remind me your name");
+        String userName = scanner.next();
+        System.out.println("What a great name you have, " + userName + "!");
+    }
 }
+
