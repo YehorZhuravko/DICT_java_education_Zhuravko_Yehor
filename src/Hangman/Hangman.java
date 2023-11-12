cbe77290d925569ea4dc7997e1e238607fe6e6f6
YehorZhuravko <y.a.zhuravko@student.khai.edu>
2023-11-12 06:51:22 +0200

    Hangman 6th stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,4 +1,5 @@
 package Hangman;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -29,25 +30,39 @@ public class Hangman {
             char userGuess = scanner.next().charAt(0);
 
             if (containsLetter(secretWord, userGuess)) {
-                System.out.println("Good guess!");
-                updateGuessedWord(secretWord, guessedWord, userGuess);
+                if (alreadyGuessed(guessedWord, userGuess)) {
+                    System.out.println("No improvements. You already guessed that letter.");
+                    attemptsLeft--;
+                } else {
+                    System.out.println("Good guess!");
+                    updateGuessedWord(secretWord, guessedWord, userGuess);
+                }
             } else {
                 System.out.println("That letter doesn't appear in the word.");
                 attemptsLeft--;
             }
 
             if (isWordGuessed(guessedWord)) {
-                System.out.println("Congratulations! You guessed the word! ");
+                System.out.println("Congratulations! You guessed the word!");
                 return;
             }
         }
 
-        System.out.println("Thanks for playing! The secret word was: " + secretWord );
+        System.out.println("Thanks for playing! The secret word was: " + secretWord);
         System.out.println("Let's see how well you do next time.");
     }
 
     public static boolean containsLetter(String word, char letter) {
         return word.indexOf(letter) != -1;
+    }
+
+    public static boolean alreadyGuessed(char[] guessedWord, char letter) {
+        for (char c : guessedWord) {
+            if (c == letter) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void updateGuessedWord(String secretWord, char[] guessedWord, char letter) {
