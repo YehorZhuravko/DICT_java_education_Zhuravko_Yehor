dfa9581dcaeaab60ff3facdb30345eeadb44c066
YehorZhuravko <y.a.zhuravko@student.khai.edu>
2023-11-12 06:08:06 +0200

    Hangman 2nd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,9 +1,30 @@
 package Hangman;
 
+import java.util.Scanner;
+
 public class Hangman {
 
     public static void main(String[] args) {
-        System.out.println("HANGMAN\nThe game will be available soon.");
+        System.out.println("HANGMAN");
+        playHangman();
+    }
+
+    public static void playHangman() {
+        String secretWord = "java";
+        char[] guessedWord = new char[secretWord.length()];
+        for (int i = 0; i < secretWord.length(); i++) {
+            guessedWord[i] = '_';
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Guess the word: ");
+        String userGuess = scanner.next();
+
+        if (userGuess.equals(secretWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }
-
