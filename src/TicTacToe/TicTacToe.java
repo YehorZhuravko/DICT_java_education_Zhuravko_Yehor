d33f8912320c74c1a6d659687b9b316959ea361d
YehorZhuravko <y.a.zhuravko@student.khai.edu>
2023-12-19 17:21:49 +0200

    TicTacToe 2-nd stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -20,15 +20,35 @@ public class TicTacToe {
         boolean game = true;
         boolean scam = true;
         while (scam){
+            System.out.println("-----");
+            System.out.println("|" + board.get("1 3") + board.get("2 3") + board.get("3 3") + "|");
+            System.out.println("|" + board.get("1 2") + board.get("2 2") + board.get("3 2") + "|");
+            System.out.println("|" + board.get("1 1") + board.get("2 1") + board.get("3 1") + "|");
+            System.out.println("-----");
+            System.out.print("\nEnter the coordinates:\n> ");
             String move = scan.nextLine();
             if (board.containsKey(move)) {
                 if(board.get(move).equals(" ")) {
                     board.put(move, XO);
-                    System.out.println("-----");
-                    System.out.println("|" + board.get("1 3") + board.get("2 3") + board.get("3 3") + "|");
-                    System.out.println("|" + board.get("1 2") + board.get("2 2") + board.get("3 2") + "|");
-                    System.out.println("|" + board.get("1 1") + board.get("2 1") + board.get("3 1") + "|");
-                    System.out.println("-----");
+                    choice -= 1;
+                    game = true;
+                }else {
+                    System.out.println("This cell is occupied! Choose another one!");
+                    game = false;
+                }
+            }else {
+                move = move.replaceAll(" ", "");
+                if(Character.isDigit(Integer.parseInt(move))) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                }else{
+                    System.out.println("You should enter numbers!");
+                }
+
+            }
+            if (choice <= 5){
+                if ((board.get("1 3").equals(board.get("2 3")) && board.get("2 3").equals(board.get("3 3")))
+                        != (board.get("1 3").equals(board.get(" ")) && board.get("2 3").equals(board.get(" ")) && board.get("3 3").equals(board.get(" ")))) {
+
                 }
             }
 
